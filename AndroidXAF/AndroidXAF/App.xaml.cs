1c94d0a21baf04c5a760d59f4d74879f8c8a9064
Abraham Qian <qianyonglian@outlook.com>
2020-09-15 16:01:14 +0800

    first commit in AndroidXAF

---
+++ AndroidXAF/AndroidXAF/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AndroidXAF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=7011b0df-8bdb-481d-97c0-ff484ec4284e;",typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
