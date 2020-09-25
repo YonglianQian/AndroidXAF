714bfa93ba9d96996fcdd5f8d1e726d5f15e0e89
Abraham Qian <qianyonglian@outlook.com>
2020-09-25 15:42:08 +0800

    add update check

--- AndroidXAF/AndroidXAF.Android/MainActivity.cs
+++ AndroidXAF/AndroidXAF.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Microsoft.AppCenter.Distribute;
 
 namespace AndroidXAF.Droid
 {
@@ -21,6 +22,8 @@ namespace AndroidXAF.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            Distribute.SetEnabledForDebuggableBuild(true);
 
             LoadApplication(new App());
         }
